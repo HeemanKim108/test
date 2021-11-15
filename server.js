8f907b1dc98d88f264846599372f9bb649d558f3
HeemanKim108 <heemankim@HeemanKimnoMacBook-Pro.local>
2021-11-15 16:19:03 +0900

    Test branch

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ app.use(express.static(__dirname + "/dist"));
 const port = process.env.PORT || 4000;
 
 app.use("/api", (req, res) => {
-    res.send("check");
+    res.send("check!");
 });
-
+//
 app.listen(port, () => {
     console.log("App listening on port" + port);
 });
